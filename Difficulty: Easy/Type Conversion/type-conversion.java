d5fbd853b8ba25d83e034d949050e44f81d42cba
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-21 13:12:38 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Type Conversion/type-conversion.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+class GFG {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        double d = sc.nextDouble();
+
+        // code here
+        int result = (int) d;
+        
+        System.out.println(result);
+    }
+}
