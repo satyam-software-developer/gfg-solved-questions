c4df174ae9709f4da2f2807539f587852054a468
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-06-30 13:30:50 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Find if +ve, -ve or 0/find-if-ve-ve-or-0.java
@@ -0,0 +1,15 @@
+class Solution {
+	void fun(int a) {
+		// code here
+		
+		if (a > 0) {
+			System.out.print("Positive");
+		} else if (a < 0) {
+			System.out.print("Negative");
+		} else {
+			
+			System.out.print("Zero");
+		}
+		
+	}
+}
