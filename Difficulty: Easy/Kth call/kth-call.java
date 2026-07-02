025cc439ca34ed2c60c4185e05465a4e437d0736
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 13:27:04 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Kth call/kth-call.java
@@ -0,0 +1,14 @@
+class Solution {
+	
+	// declare the variable count here
+	int count = 0;
+	
+	void utility() {
+		
+		// increases the value of count by 1
+		count++;
+		
+		// print count
+		System.out.print(count);
+	}
+}
