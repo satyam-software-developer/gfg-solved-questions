5a70ee38893670e127bd431782ff6d1e62c04f6f
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-02 14:42:11 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Scientific Format/scientific-format.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class GFG {
+	public static void main(String[] args) {
+		// code here
+		Scanner sc = new Scanner(System.in);
+		
+		Double a = sc.nextDouble();
+		
+		System.out.printf("%.4e", a);
+		
+	}
+}
