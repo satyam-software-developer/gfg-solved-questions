dc3aabd7f28571e07619c36a75489ba019810567
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 12:38:41 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Find Volume/find-volume.java
@@ -0,0 +1,14 @@
+class Solution {
+	
+	// Write the first function to
+	int volume(int s) {
+		// return the volume of cube
+		return s * s * s;
+	}
+	
+	// Write the second function to
+	int volume(int l, int w, int h) {
+		// return the volume of the rectangular box
+		return l * w * h;
+	}
+}
