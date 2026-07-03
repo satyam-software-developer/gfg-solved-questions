bec91e936d08012958dcf23b474705ae6ad122e7
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-03 13:03:33 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/C++ Classes Introduction/c-classes-introduction.cpp
@@ -0,0 +1,46 @@
+class CollegeCourse {
+	string courseID;
+	char grade;
+	int credits;
+	int gradePoints;
+	float honorPoints;
+	
+	public:
+	void set_CourseId(string CID) {
+		courseID = CID;
+	}
+	
+	void set_Grade(char g) {
+		grade = toupper(g);
+	}
+	
+	void set_Credit(int cr) {
+		credits = cr;
+	}
+	
+	int calculateGradePoints(char g) {
+		g = toupper(g);
+		
+		if (g == 'A')
+			return 10;
+		if (g == 'B')
+			return 9;
+		if (g == 'C')
+			return 8;
+		if (g == 'D')
+			return 7;
+		if (g == 'E')
+			return 6;
+		return 5;
+	}
+	
+	float calculateHonorPoints(int gp, int cr) {
+		return gp * cr;
+	}
+	
+	void display() {
+		gradePoints = calculateGradePoints(grade);
+		honorPoints = calculateHonorPoints(gradePoints, credits);
+		cout << gradePoints << " " << honorPoints << endl;
+	}
+};
