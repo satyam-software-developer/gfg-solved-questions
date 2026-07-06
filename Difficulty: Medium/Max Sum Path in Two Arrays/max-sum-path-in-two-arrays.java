4f413a32914e2ebb32a362f7934860bf96035ff5
Satyam Kumar <94899903+satyam-software-developer@users.noreply.github.com>
2026-07-06 23:33:29 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Max Sum Path in Two Arrays/max-sum-path-in-two-arrays.java
@@ -0,0 +1,32 @@
+class Solution {
+	public int maxPathSum(int[] a, int[] b) {
+		int i = 0, j = 0;
+		long sum1 = 0, sum2 = 0, result = 0;
+		
+		while (i < a.length && j < b.length) {
+			if (a[i] < b[j]) {
+				sum1 += a[i++];
+			} else if (a[i] > b[j]) {
+				sum2 += b[j++];
+			} else {
+				result += Math.max(sum1, sum2) + a[i];
+				sum1 = 0;
+				sum2 = 0;
+				i++;
+				j++;
+			}
+		}
+		
+		while (i < a.length) {
+			sum1 += a[i++];
+		}
+		
+		while (j < b.length) {
+			sum2 += b[j++];
+		}
+		
+		result += Math.max(sum1, sum2);
+		
+		return (int) result;
+	}
+}
